07a908bac17c06174cbbd5ffbe76d97cf52ecc9c
Daniel En-Wenn Ho <daniel.e.ho@gmail.com>
2004-06-26 02:54:22 +0000

    *** empty log message ***

--- inst/doc/papers/koch/fn.R
+++ inst/doc/papers/koch/fn.R
@@ -1,3 +1,4 @@
+#simulating posterior draws
 draw.ols <- function(ols1,sims=1000){
   rinvchisq <- function (n, df, scale = 1/df){
     if ((length(scale) != 1) & (length(scale) != n))
@@ -8,7 +9,7 @@ draw.ols <- function(ols1,sims=1000){
       stop("scale must be greater than zero")
     return((df * scale)/rchisq(n, df = df)) }
   s2 <- summary(ols1)$sigma^2 
-  bhat <- ols1$coefficients 
+  bhat <- na.omit(ols1$coefficients)
   k <- length(bhat)
   n <- nrow(model.matrix(ols1))
   vhat <- summary(ols1)$cov.unscaled 
@@ -31,36 +32,49 @@ doverlay <- function(x1, x0, xlab = "", main = "", lines = FALSE, ...)
   dx0 <- density(x0, from = minobs, to = maxobs)
     matplot(dx0$x, cbind(dx1$y, dx0$y), type = "l", ylab = "Density", 
             xlab = xlab, main = main, ...)
-  legend(0.7, max(c(dx1$y, dx0$y)), lty = 1:2, col = 1:2, 
-         legend = c("Treatment", "Control"), cex=0.5)
+  legend(-0.2, max(c(dx1$y, dx0$y)), lty = 1:2, col = 1:2, 
+         legend = c("Full Data", "Matched Data"), cex=0.5)
 }
 
 #model adjustment
-ols.ate <- function(ols.m,sims=1000){
+ols.ate <- function(ols.m,sims=1000,treat){
   b.sim <- as.matrix(draw.ols(ols.m,sims=sims))
-  b.sim <- b.sim[,1:(ncol(b.sim)-1)]
-  XX <- as.matrix(cbind(1,match.data(m1,"treat")[,m1$covariates]))
-  y.hat <- XX%*%t(b.sim)
+  b.sim <- b.sim[,1:(ncol(b.sim)-1),drop=F]
+  dta <- eval(ols.m$call$data,parent.frame())
+  tt <- dta[,treat]
+  dta[,treat] <- 1-dta[,treat]
+  xx <- model.matrix(eval(ols.m$call$formula),data=dta)
+  xx <- xx[,!is.na(coef(ols.m))]
+  y.hat <- xx%*%t(b.sim)
   ate <- matrix(0,sims,1)
   y.obs <- model.frame(ols.m)[,1]
   nn <- length(y.obs)
   if(nn==nrow(y.hat)){
     yy <- matrix(y.obs,nn,sims)
+  } else {
+    stop("Dimensions of yobs and yhat off")
   }
-  obj <- apply(yy-y.hat,2,mean)
+  ss <- (1-tt)*y.hat+tt*yy-(tt*y.hat+(1-tt)*yy)
+  obj <- apply(ss,2,mean)
   obj
 }
 
+#dta <- dta.full
+#dta <- dta.match
 #wrapper for zelig
-zsim <- function(fml,dta){
-  tt <- dta$dviswom
-  z.out <- zelig(fml, data = dta, model="ls")
-  x1 <- setx(z.out, data = dta[tt==1,],cond=T)
-  x0 <- setx(z.out, data = dta[tt==0,],cond=T)
-  x1[,dimnames(x.out)[[2]]=="dviswom"] <- 0
-  x0[,dimnames(x.out)[[2]]=="dviswom"] <- 1
-  s1 <- sim(z.out, x = x1, fn=NULL)
-  s0 <- sim(z.out, x = x0, fn=NULL)
-  ate <- c(s1$qi$ate.ev,-s0$qi$ate.ev)
-  ate
+zsim <- function(fml,dta,num=1000,zz=T,treat="dviswom"){
+  if(zz){
+    tt <- dta$dviswom
+    z.out <- zelig(fml, data = dta, model="ls")
+    dta$dviswom <- 1-dta$dviswom
+    x1 <- setx(z.out, data = dta[tt==1,],cond=T)
+    x0 <- setx(z.out, data = dta[tt==0,],cond=T)
+    s1 <- sim(z.out, x = x1, fn=NULL, num=1000)
+    s0 <- sim(z.out, x = x0, fn=NULL, num=1000)
+    ate <- c(s1$qi$ate.ev,-s0$qi$ate.ev)   
+  } else {
+    ols.m <- lm(fml,data=dta)
+    ate <- ols.ate(ols.m,sim=num,treat)
+  }
+  return(ate)
 }
--- inst/doc/papers/koch/koch.R
+++ inst/doc/papers/koch/koch.R
@@ -9,19 +9,112 @@ setwd("c:/match/docs/papers/koch/")
 source("fn.R")
 dta <- read.dta("genharvard.dta")
 
+#==================================
+# Model 1 (Democrats)
+#==================================
+
 #creating full dataset
 dta.full <- subset(dta,select=c(pdcanid, dviswom , dvisman , demcan1 ,
                      dempty , rideo, dproj , demft , aware))
 dta.full <- na.omit(dta.full)
-#now matching (but here we're using the full dataset)
+#now creating matched data
 m1 <- matchit(dviswom ~ demcan1 + dempty + rideo +
            dproj + demft + aware, data=dta.full)
 dta.match <- match.data(m1)
 
-fml <- as.formula(pdcanid ~ dviswom + demcan1 +
-           dempty + rideo + dproj + demft + aware)
-fml <- as.formula(pdcanid ~ dviswom + I(dviswom*rideo) + demcan1 +
-           dempty + rideo + dproj + demft + aware)
-foo <- zsim(fml,dta.full)
-foo <- zsim(fml,dta.match)
+#creating possible RHS specifications
+xvar <- names(dta.full)[2:length(names(dta.full))]
+xvar <- xvar[xvar!="dvisman"]
 
+#another way to show the curse of dimensionality
+foo<-apply(dta.full[,xvar],2,table)
+kk <- 1
+for(i in 1:length(foo)){
+  kk <- kk*length(foo[[i]])
+}
+# really hard to summarize 380108484 possible covariate cells!!
+
+k <- length(xvar)
+for(i in 1:k){
+  for(j in i:k){
+    xvar <- c(xvar,paste("I(",xvar[i],"*",xvar[j],")",sep=""))
+  }
+}
+
+totk <- length(xvar)
+tot <- 0
+for(i in 1:totk){
+  tot <- tot+choose(totk,i)
+}
+# 3.436e+10 total specifications
+
+sims <- 1000
+ate.full <- ate.match <- NULL
+simseq <- floor(seq(sims/10,sims,sims/10))
+for(i in 1:sims){
+  sp <- FALSE
+  while(!sp){ #only sampling specns with ttt on RHS
+    xs <- xvar[sample(1:totk,size=sample(1:totk,size=1),replace=F)]
+    if(length(grep("dviswom",xs))!=0){sp <- TRUE}
+  }
+  fml <- as.formula(paste("pdcanid ~", paste(xs, collapse= " + ")))
+  ff <- zsim(fml,dta.full,num=10,zz=F)
+  ate.full <- c(ate.full,ff)
+  mm <- zsim(fml,dta.match,num=10,zz=F)
+  ate.match <- c(ate.match,mm)
+  if(i%in%simseq){
+    cat(10*which(simseq==i),"%...",sep="")
+    if(i==sims){cat("Done\n")}
+  }
+}
+doverlay(ate.full,ate.match)
+
+
+
+#==================================
+# Model 3 (Republicans)
+#==================================
+
+dta.full <- subset(dta,select=c(prcanid, rviswom, rvisman, repcan1 , goppty ,
+              rideo , rproj , repft , aware))
+dta.full <- na.omit(dta.full)
+m1 <- matchit(rviswom ~ rvisman + repcan1 + goppty + rideo + rproj +
+              repft + aware, data=dta.full, caliper=0.1)
+dta.match <- match.data(m1)
+
+#creating possible RHS specifications
+xvar <- names(dta.full)[2:length(names(dta.full))]
+#xvar <- xvar[xvar!="rvisman"]
+
+k <- length(xvar)
+for(i in 1:k){
+  for(j in i:k){
+    xvar <- c(xvar,paste("I(",xvar[i],"*",xvar[j],")",sep=""))
+  }
+}
+totk <- length(xvar)
+tot <- 0
+for(i in 1:totk){
+  tot <- tot+choose(totk,i)
+}
+
+sims <- 1000
+ate.full <- ate.match <- NULL
+simseq <- floor(seq(sims/10,sims,sims/10))
+for(i in 1:sims){
+  sp <- FALSE
+  while(!sp){ #only sampling specns with ttt on RHS
+    xs <- xvar[sample(1:totk,size=sample(1:totk,size=1),replace=F)]
+    if(length(grep("rviswom",xs))!=0){sp <- TRUE}
+  }
+  fml <- as.formula(paste("prcanid ~", paste(xs, collapse= " + ")))
+  ff <- zsim(fml,dta.full,num=1,zz=F,treat="rviswom")
+  ate.full <- c(ate.full,ff)
+  mm <- zsim(fml,dta.match,num=1,zz=F,treat="rviswom")
+  ate.match <- c(ate.match,mm)
+  if(i%in%simseq){
+    cat(10*which(simseq==i),"%...",sep="")
+    if(i==sims){cat("Done\n")}
+  }
+}
+doverlay(ate.full,ate.match)
