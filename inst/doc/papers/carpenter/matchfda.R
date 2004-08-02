9fd1151de79d23ee05e47805fe405ee49e111653
Kosuke Imai <kimai@Princeton.Edu>
2004-08-02 23:01:45 +0000

    edited

--- inst/doc/papers/carpenter/matchfda.R
+++ inst/doc/papers/carpenter/matchfda.R
@@ -4,7 +4,7 @@ data$treat <- data$demsnmaj
 sink("matchfda.out")
 library(Matchit)
 mout <- matchit(treat ~ orderent + stafcder + prevgenx + lethal +
-                deathrt1 + hosp01 + I(hospdisc/10000) +
+                deathrt1 + hosp01 + I(hospdisc/1000000) +
                 hhosleng + femdiz01 + mandiz01 + peddiz01 + acutediz +
                 orphdum + natreg + I(natregsq/1000) +  wpnoavg3 +
                 sqrt(wpnoavg3) + vandavg3 + condavg3, data=data,
@@ -18,14 +18,14 @@ fullmodel <- as.formula(Surv(acttime, d) ~ treat + prevgenx + lethal +
                         deathrt1 + acutediz + hosp01 + I(hospdisc/10000) +
                         hhosleng + femdiz01 + mandiz01 + peddiz01 +
                         orphdum + natreg + I(natregsq/1000) + vandavg3 + 
-                        wpnoavg3 + sqrt(wpnoavg3) + condavg3 +
+                        wpnoavg3 + condavg3 +
                         orderent + stafcder)
+
 res <- zelig(fullmodel, data=data, model="lognorm")
 mres <- zelig(fullmodel, data=mdata, model="lognorm")
 xvars <- names(res$coefficients)
 xvars <- xvars[3:length(xvars)]
 start <- paste("Surv(acttime, d) ~ treat")
-coef <- mcoef <- NULL
 
 library(combinat)
 N <- 1
@@ -34,7 +34,7 @@ for (i in N:(length(xvars)-1))
   total <- total + ncol(combn(xvars, i)) 
 cat("\n I'm going to run", total, "regressions!\n")
     
-coef <- mcoef <- rep(0,total)
+ate <- mate <- rep(0,total)
 counter <- 1
 cat("start", date(), "\n")
 for (i in N:(length(xvars)-1)) {
@@ -44,16 +44,32 @@ for (i in N:(length(xvars)-1)) {
     for (k in 1:i) 
       ftmp <- paste(ftmp, "+", allsubset[k,j])
     ftmp <- as.formula(ftmp)
-    tmp <- survreg(ftmp,  data = data, dist="lognormal") 
-    coef[counter] <- tmp$coefficient["treat"]
+    tmp <- survreg(ftmp,  data = data, dist="lognormal")
+    x0 <- x1 <- model.matrix(tmp)
+    x0[,"treat"] <- 0
+    x1[,"treat"] <- 1
+    ate[counter] <- mean(exp(x1%*%tmp$coefficient+0.5*tmp$scale^2) -
+                         exp(x0%*%tmp$coefficient+0.5*tmp$scale^2))
     tmp <- survreg(ftmp, data = mdata, dist="lognormal") 
-    mcoef[counter] <- tmp$coefficient["treat"]
+    x0 <- x1 <- model.matrix(tmp)
+    x0[,"treat"] <- 0
+    x1[,"treat"] <- 1
+    mate[counter] <- mean(exp(x1%*%tmp$coefficient+0.5*tmp$scale^2) -
+                          exp(x0%*%tmp$coefficient+0.5*tmp$scale^2))
     counter <- counter + 1
   }
   cat(i,"covariates:",date(),"\n")
 }
-coef[length(coef)] <- res$coefficients["treat"] 
-mcoef[length(mcoef)] <- mres$coefficients["treat"] 
+x0 <- x1 <- model.matrix(res)
+x0[,"treat"] <- 0
+x1[,"treat"] <- 1
+ate[length(ate)] <- mean(exp(x1%*%res$coefficient+0.5*tmp$scale^2) -
+                         exp(x0%*%res$coefficient+0.5*tmp$scale^2))
+x0 <- x1 <- model.matrix(mres)
+x0[,"treat"] <- 0
+x1[,"treat"] <- 1
+mate[length(mate)] <- mean(exp(x1%*%mres$coefficient+0.5*tmp$scale^2) -
+                           exp(x0%*%mres$coefficient+0.5*tmp$scale^2))
 
 save.image("matchfda.RData")
 sink()
