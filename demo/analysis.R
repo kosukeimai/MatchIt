75e8bab99dd9889665875237b78ff0d134d8593f
Kosuke Imai <kimai@Princeton.Edu>
2004-08-15 03:40:18 +0000

    new name

---
+++ demo/analysis.R
@@ -0,0 +1,68 @@
+# Examples of analyses, with and without Zelig
+# Without zelig
+
+data(lalonde)
+
+# First just neyman on matched sample
+print("Neyman estimates using a matched sample")
+match.out1 <- matchit(treat ~ age + educ + black + hispan + nodegree +
+                      married + re74 + re75, data = lalonde)
+neyman.out <- neyman(re78, match.out1)
+print(neyman.out)
+print(summary(neyman.out))
+silent <- readline("\nPress <return> to continue: ")
+
+# Neyman using propensity score subclasses
+print("Neyman estimates with propensity score subclassification")
+match.out1s <- matchit(treat ~ age + educ + black + hispan + nodegree
+                       + married + re74 + re75, data = lalonde, subclass=4)
+neyman.outs <- neyman(re78, match.out1s)
+print(neyman.outs)
+print(summary(neyman.outs))
+silent <- readline("\nPress <return> to continue: ")
+
+# Neyman using exact matching
+print("Neyman estimates using exact matching")
+match.out1e <- matchit(treat ~ black + hispan, data = lalonde, exact=TRUE)
+neyman.oute <- neyman(re78, match.out1e)
+print(neyman.oute)
+print(summary(neyman.oute))
+silent <- readline("\nPress <return> to continue: ")
+
+# Run linear regression on matched data: use match.out1
+print("Linear regression on matched data set")
+lm.1 <- lm(re78 ~ treat + pscore, data=match.data(match.out1))
+
+print(summary(lm.1)$coef[2,])
+silent <- readline("\nPress <return> to continue: ")
+
+# Zelig part from zelig documentation (match.R in demo directory)
+foo <- .find.package("Zelig",quiet=T)
+if(length(foo)==0){
+  cat("Zelig is not installed.  \nFor further illustrations of analyses,\nyou may install Zelig from \nhttp://gking.harvard.edu/zelig/\n")
+} else {
+  library(Zelig)
+
+  ## an example for propensity score matching
+  print("Using Zelig for analysis")
+  z.out1 <- zelig(re78 ~ pscore, data = match.data(match.out1,
+                                   "control"), model = "ls")
+  x.out1 <- setx(z.out1, data = match.data(match.out1, "treat"), cond =
+                 TRUE)
+  s.out1 <- sim(z.out1, x = x.out1)
+  print(summary(s.out1))
+  user.prompt()
+  
+  ## an example for subclassification
+  print("Using Zelig for analysis, with propensity score subclassification")
+  z.out2 <- zelig(re78 ~ pscore, data = match.data(match.out1s,
+                                   "control"), model="ls", by="psclass")
+  x.out2 <- setx(z.out2, data = match.data(match.out1s, "treat"), cond =
+                 TRUE)
+  s.out2 <- sim(z.out2, x = x.out2, num = 100)
+  print(summary(s.out2)) # overall results
+  print(summary(s.out2, subset = 1)) # subclass 1
+  print(summary(s.out2, subset = 2)) # subclass 2
+  print(summary(s.out2, subset = 3)) # subclass 3
+  user.prompt()
+}
---
+++ demo/lalonde.R
@@ -0,0 +1,97 @@
+# MatchIt Demo script with Lalonde data
+
+data(lalonde)
+
+#Exact matching
+foo1 <- matchit(treat ~ black + hispan, exact=TRUE, data=lalonde)
+print(foo1)
+summary(foo1)
+summary(foo1,verbose=T)
+row.names(foo1$data)[foo1$psclass==1][1:20]
+foo1$data[foo1$psclass==2,c("black", "hispan")][1:20,]
+silent <- readline("\nPress <return> to continue: ")
+
+#Propensity score matching
+foo2 <- matchit(treat ~ re74 + re75, data=lalonde)
+print(foo2)
+summary(foo2)
+summary(foo2, verbose=T)
+plot(foo2)
+
+#Propensity score with exact restriction
+foo3 <- matchit(treat ~ re74 + re75, exact=c("black","hispan"), data=lalonde)
+print(foo3)
+sum.foo3 <- summary(foo3)
+sum.foo3$sum.all[c("hispan","black"),]
+sum.foo3$sum.matched[c("hispan","black"),]
+dta.matched <- match.data(foo3)
+dta.treated <- match.data(foo3, group = "treat")
+dta.control <- match.data(foo3, group = "control")
+
+#Replication of Dehejia & Wahba
+foo <- matchit(treat ~ age + I(age^2) + educ + I(educ^2) + black +
+               hispan + married + nodegree + re74 + I(re74^2) +
+               re75 + I(re75^2) + I(as.numeric(re74==0)) +
+               I(as.numeric(re75==0)), data=lalonde, replace=TRUE,
+               discard=1)
+
+#Non-matched and discarded units
+foo$in.sample[!foo$in.sample]
+foo$psweights[foo$in.sample & foo$psweights==0]
+sdisc <- apply(lalonde[!foo$in.sample,],2,mean) #taking column-wise means
+streat <- apply(lalonde[lalonde$treat==1,],2,mean)
+round(rbind(sdisc,streat),2) #rounding
+
+#Look at weights
+print(foo$psweights[foo$data$treat==1][1:10])
+print(foo$psweights[foo$data$treat==0][1:20])
+print((foo$psweights[foo$data$treat==0]*sum(foo$psweights[foo$data$treat==1])/sum(foo$psweights[foo$data$treat==0]))[1:20])
+
+#Subclassification
+foo1 <- matchit(treat ~ age + educ + black + hispan + married +
+                nodegree + re74 + re75, data=lalonde, replace=TRUE,
+                subclass=6)
+foo2 <- matchit(treat ~ age + educ + black + hispan + married +
+                nodegree + re74 + re75, data=lalonde, nearest=FALSE,
+                replace=TRUE, subclass=6)
+print(foo2)
+plot(foo2)
+
+#Exact matching on all covariates
+foo <- matchit(treat ~ age + educ + black + hispan + married +
+               nodegree + re74 + re75, data=lalonde, exact=TRUE)
+summary(foo)
+
+# Caliper matching
+foo <- matchit(treat ~ age + educ + black + hispan + married +
+               nodegree + re74 + re75, data=lalonde, caliper=0.25,
+               replace=TRUE)
+
+#Mahalanobis matching
+foo <- matchit(treat ~ age + educ, data=lalonde, mahvars = c("age", "educ"),
+               caliper=Inf, replace=TRUE)
+
+#Assignment Model specification
+foo1 <- matchit(treat~educ+re74+re75,data=lalonde,model="probit")
+foo2 <- matchit(treat~educ+re74+re75,data=lalonde,model="nnet")
+foo3 <- matchit(treat~educ+re74+re75,data=lalonde,model="GAM")
+foo4 <- matchit(treat~educ+re74+re75,data=lalonde,model="cart")
+
+#Using Observation Names
+test <- lalonde[1:4,] #taking a lalonde subset
+row.names(test) <- c("Dan","Kosuke","Liz","Gary") #assigning row names
+print(test)
+
+#Matching on One Covariate
+index <- rep(TRUE,nrow(lalonde)) #keeping all units
+names(index) <- row.names(lalonde) #assigning obs names
+treat <- lalonde$treat
+names(treat) <- row.names(lalonde)
+re75 <- lalonde$re75
+names(re75) <- row.names(lalonde)
+foo <- matchdef(treat~re75,index,pscore=re75,replace=TRUE,data=lalonde)
+cbind(lalonde[row.names(foo$match.matrix),]$re75,
+      lalonde[as.vector(foo$match.matrix[,1]),]$re75) #matched re75
+plot(lalonde[row.names(foo$match.matrix),]$re75,
+     lalonde[as.vector(foo$match.matrix[,1]),]$re75, pch=16,
+     xlab="Treated", ylab="Controls") #plotting matched data
