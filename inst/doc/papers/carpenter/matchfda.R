793ee5f73957afed7a1450a9f0adb4d947e8ae4f
Kosuke Imai <kimai@Princeton.Edu>
2004-09-06 19:43:35 +0000

    added point estimate and se

--- inst/doc/papers/carpenter/matchfda.R
+++ inst/doc/papers/carpenter/matchfda.R
@@ -27,6 +27,7 @@ xvars <- names(res$coefficients)
 xvars <- xvars[3:length(xvars)]
 start <- paste("Surv(acttime, d) ~ treat")
 
+
 library(combinat)
 N <- 1
 total <- 1
@@ -48,28 +49,50 @@ for (i in N:(length(xvars)-1)) {
     x0 <- x1 <- model.matrix(tmp)
     x0[,"treat"] <- 0
     x1[,"treat"] <- 1
-    ate[counter] <- mean(exp(x1%*%tmp$coefficient+0.5*tmp$scale^2) -
-                         exp(x0%*%tmp$coefficient+0.5*tmp$scale^2))
+    ate[counter] <- mean(exp(x1%*%tmp$coefficients+0.5*tmp$scale^2) -
+                         exp(x0%*%tmp$coefficients+0.5*tmp$scale^2))
     tmp <- survreg(ftmp, data = mdata, dist="lognormal") 
     x0 <- x1 <- model.matrix(tmp)
     x0[,"treat"] <- 0
     x1[,"treat"] <- 1
-    mate[counter] <- mean(exp(x1%*%tmp$coefficient+0.5*tmp$scale^2) -
-                          exp(x0%*%tmp$coefficient+0.5*tmp$scale^2))
+    mate[counter] <- mean(exp(x1%*%tmp$coefficients+0.5*tmp$scale^2) -
+                          exp(x0%*%tmp$coefficients+0.5*tmp$scale^2))
     counter <- counter + 1
   }
   cat(i,"covariates:",date(),"\n")
 }
+
+## using the full model
 x0 <- x1 <- model.matrix(res)
 x0[,"treat"] <- 0
 x1[,"treat"] <- 1
-ate[length(ate)] <- mean(exp(x1%*%res$coefficient+0.5*tmp$scale^2) -
-                         exp(x0%*%res$coefficient+0.5*tmp$scale^2))
+ate[length(ate)] <- mean(exp(x1%*%res$coefficients+0.5*tmp$scale^2) -
+                         exp(x0%*%res$coefficients+0.5*tmp$scale^2))
+sims <- 5000
+library(MASS)
+param <- mvrnorm(sims, mu=c(res$coefficients, log(res$scale)),
+                 Sigma=res$var) 
+sate <- exp(param[,-ncol(param)]%*%t(x1) +
+            0.5*exp(param[,ncol(param)])^2) -
+  exp(param[,-ncol(param)]%*%t(x0) +
+      0.5*exp(param[,ncol(param)])^2)
+
+est <- c(mean(apply(sate, 1, mean)), sd(apply(sate, 1, mean)))
+
 x0 <- x1 <- model.matrix(mres)
 x0[,"treat"] <- 0
 x1[,"treat"] <- 1
-mate[length(mate)] <- mean(exp(x1%*%mres$coefficient+0.5*tmp$scale^2) -
-                           exp(x0%*%mres$coefficient+0.5*tmp$scale^2))
+mate[length(mate)] <- mean(exp(x1%*%mres$coefficients+0.5*tmp$scale^2) -
+                           exp(x0%*%mres$coefficients+0.5*tmp$scale^2))
+
+mparam <- mvrnorm(sims, mu=c(mres$coefficients, log(mres$scale)),
+                  Sigma=mres$var) 
+msate <- exp(mparam[,-ncol(mparam)]%*%t(x1) +
+             0.5*exp(mparam[,ncol(mparam)])^2) -
+  exp(mparam[,-ncol(mparam)]%*%t(x0) +
+      0.5*exp(mparam[,ncol(mparam)])^2)
+
+mest <- c(mean(apply(msate, 1, mean)), sd(apply(msate, 1, mean)))
 
 save.image("matchfda.RData")
 sink()
