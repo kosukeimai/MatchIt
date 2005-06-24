6e4be4c0b18fac4a01ad8d6aa9dc81fe940992d8
Daniel En-Wenn Ho <daniel.e.ho@gmail.com>
2005-06-24 00:55:37 +0000

    *** empty log message ***

---
+++ R/print.matchit.default.R
@@ -0,0 +1,11 @@
+print.matchit.default <- function(obj){
+  cat("\nCall: ", deparse(obj$call),"\n",sep = "")
+  cat("\nSample sizes:\n")
+  nn <- rbind(table(obj$data$treat),
+              table(obj$matched,obj$data$treat)[2:1,])
+  dimnames(nn) <- list(c("Full","Matched","Discarded"),
+                       c("Control","Treated"))
+  print.table(nn)
+  invisible(obj)
+  cat("\n")
+}
