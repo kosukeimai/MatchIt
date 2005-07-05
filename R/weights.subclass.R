a74e716d3c2351ace099086e38852cbb98e2b0fd
Kosuke Imai <kimai@Princeton.Edu>
2005-07-05 14:24:21 +0000

    some clean up

--- R/weights.subclass.R
+++ R/weights.subclass.R
@@ -11,27 +11,26 @@ weights.subclass <-function(psclass, treat) {
  
   classes <- unique(psclass)
   for(j in classes){
-        qn0 <- sum(treat==0 & psclass==j)
-        qn1 <- sum(treat==1 & psclass==j)
-        weights[treat==0 & psclass==j] <- qn1/qn0
-      }
-    if (sum(weights[treat==0], na.rm=T)==0) {
-      weights[treat==0] <- rep(0, length(weights[clabels]))
-    } else {
-      # Number of C units that were matched to at least 1 T
-      num.cs <- sum(weights[clabels][psclass[clabels]!=0]>0)
-      weights[clabels][psclass[clabels]!=0] <- 
-	weights[clabels][psclass[clabels]!=0]*num.cs/sum(weights[clabels][psclass[clabels]!=0])
-    }
-
-  if (sum(weights)==0) {
-    stop("Error: No units were matched")
-  } else if (sum(weights[tlabels])==0){
-    stop("No treated units were matched",call.=FALSE)
-  } else if (sum(weights[clabels])==0){
-    stop("No control units were matched",call.=FALSE)
+    qn0 <- sum(treat==0 & psclass==j)
+    qn1 <- sum(treat==1 & psclass==j)
+    weights[treat==0 & psclass==j] <- qn1/qn0
   }
-  cat("Done\n")
+  if (sum(weights[treat==0], na.rm=T)==0)
+    weights[treat==0] <- rep(0, length(weights[clabels]))
+  else {
+    ## Number of C units that were matched to at least 1 T
+    num.cs <- sum(weights[clabels][psclass[clabels]!=0]>0)
+    weights[clabels][psclass[clabels]!=0] <- 
+      weights[clabels][psclass[clabels]!=0]*num.cs/sum(weights[clabels][psclass[clabels]!=0])
+  }
+  
+  if (sum(weights)==0) 
+    stop("No units were matched")
+  else if (sum(weights[tlabels])==0)
+    stop("No treated units were matched")
+  else if (sum(weights[clabels])==0)
+    stop("No control units were matched")
+  
   return(list(psweights=weights))
 }
 
