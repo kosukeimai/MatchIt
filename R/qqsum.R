9131fb0419b604bfb9753088bdef803829a33f91
Daniel En-Wenn Ho <daniel.e.ho@gmail.com>
2005-07-11 00:01:13 +0000

    *** empty log message ***

---
+++ R/qqsum.R
@@ -0,0 +1,19 @@
+## Function for QQ summary stats
+qqsum <- function (x, y){
+  sx <- sort(x)
+  sy <- sort(y)
+  lenx <- length(sx)
+  leny <- length(sy)
+  if (leny < lenx) 
+    sx <- approx(1:lenx, sx, n = leny)$y
+  if (leny > lenx) 
+    sy <- approx(1:leny, sy, n = lenx)$y
+  dxy <- sqrt(2)*abs(sx-sy)
+  meandiff <- mean(dxy)
+  meddiff <- as.numeric(median(dxy))
+  maxdiff <- max(dxy)
+  invisible(list(x = sx, y = sy,
+                 meandiff=meandiff,
+                 meddiff = meddiff,
+                 maxdiff = maxdiff))
+}
