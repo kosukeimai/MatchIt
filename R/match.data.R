47db0bafc2ebce4278b9553f978a34143548e639
Kosuke Imai <kimai@Princeton.Edu>
2005-07-13 00:36:23 +0000

    first version

--- R/match.data.R
+++ R/match.data.R
@@ -1,18 +1,36 @@
 match.data <- function(object, group = "all", distance = "distance",
-                       weights = "weights") {
+                       weights = "weights", subclass = "subclass") {
   data <- eval(object$call$data)
   treat <- object$treat
+  wt <- object$weights
   vars <- names(data)
   if (distance %in% vars)
     stop("invalid input for distance. choose a different name.")
+  else if (!is.null(object$distance)) {
+    dta <- data.frame(cbind(data, object$distance))
+    names(dta) <- c(names(data), distance)
+    data <- dta
+  }
   if (weights %in% vars)
     stop("invalid input for weights. choose a different name.")
+  else if (!is.null(object$weights)){
+    dta <- data.frame(cbind(data, object$weights))
+    names(dta) <- c(names(data), weights)
+    data <- dta
+  }
+  if (subclass %in% vars)
+    stop("invalid input for subclass. choose a different name.")
+  else if (!is.null(object$subclass)){
+    dta <- data.frame(cbind(data, object$subclass))
+    names(dta) <- c(names(data), subclass)
+    data <- dta
+  }
   if (group == "all")
-    return(data[object$matched,])
+    return(data[wt > 0,])
   else if (group == "treat")
-    return(data[object$matched & treat == 1,])
+    return(data[wt > 0 & treat == 1,])
   else if (group == "control")
-    return(data[object$matched & treat == 0,])
+    return(data[wt > 0 & treat == 0,])
   else
     stop("error: invalid input for group.")
 }
