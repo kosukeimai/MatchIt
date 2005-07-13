260954a42ec48f7e732a67926cbaa32645fddf8b
Daniel En-Wenn Ho <daniel.e.ho@gmail.com>
2005-07-13 21:23:31 +0000

    *** empty log message ***

---
+++ R/jitter.pscore.R
@@ -0,0 +1,26 @@
+jitter.pscore <- function(obj){
+  treat <- obj$treat
+  pscore <- obj$distance
+  matched <- obj$weights!=0
+  q.cut <- obj$q.cut
+  jitp <- jitter(rep(1,length(treat)),factor=6)-(treat==0)
+  cwt <- sqrt(weights)
+  plot(pscore,xlim=range(na.omit(pscore)),ylim=c(-1,2),
+       type="n",ylab="",xlab="Propensity Score",
+       axes=F,main="Distribution of Propensity Scores")
+  if(!is.null(q.cut)){abline(v=q.cut,col="grey",lty=1)}
+  points(pscore[treat==1&weights!=0],jitp[treat==1&weights!=0],
+         pch=18,cex=cwt[treat==1&weights!=0])
+  points(pscore[treat==1&weights==0],jitp[treat==1&weights==0],
+         pch=5,col="grey",cex=0.5)
+  points(pscore[treat==0&weights!=0],jitp[treat==0&weights!=0],
+         pch=18,cex=cwt[treat==0&weights!=0])
+  points(pscore[treat==0&weights==0],jitp[treat==0&weights==0],
+         pch=5,col="grey",cex=0.5)
+  axis(1)
+  text(sum(range(na.omit(pscore)))/2,1.5,"Treatment Units")
+  text(sum(range(na.omit(pscore)))/2,-0.5,"Control Units")
+  box()
+  print("To identify the units, use first mouse button; to stop, use second.")
+  identify(pscore,jitp,names(treat))
+}
---
+++ R/matchit.qqplot.R
@@ -0,0 +1,66 @@
+matchit.qqplot <- function(obj,discrete.cutoff){
+  covariates  <- obj$X
+  treat <- obj$treat
+  pscore <- obj$distance
+  matched <- obj$weights!=0
+  nn <- dimnames(covariates)[[2]]
+  nc <- length(nn)
+  covariates <- data.matrix(covariates)
+  oma <- c(4, 4, 6, 4)
+  opar <- par(mfrow = c(3, 3), mar = rep.int(1/2, 4), oma = oma, ask=T)
+  on.exit(par(opar))
+  for (i in 1:nc){
+    xi <- covariates[,i]
+    ni <- nn[i]
+    plot(xi,type="n",axes=F)
+    if(((i-1)%%3)==0){
+      mtext("QQ Plots", 3, 3, TRUE, 0.5, cex=1.2,font=2)
+      mtext("Raw", 3, 1, TRUE, 0.5, cex=1.2,font = 1)
+      mtext("Matched", 3, 1, TRUE, 0.83, cex=1.2,font = 1)
+      mtext("Control Units", 1, 0, TRUE, 2/3, cex=1,font = 1)
+      mtext("Treated Units", 4, 0, TRUE, 0.5, cex=1,font = 1)
+    }
+    par(usr = c(0, 1, 0, 1))
+    l.wid <- strwidth(nn, "user")
+    cex.labels <- max(0.8, min(2, 0.9/max(l.wid)))
+    text(0.5,0.5,ni,cex=cex.labels)
+    if(length(table(xi))>discrete.cutoff){
+      rr <- range(xi)
+      qqplot(xi[treat==0],xi[treat==1],
+             xlim=rr,ylim=rr,axes=F,ylab="",
+             xlab="")
+      abline(a=0,b=1)
+      abline(a=(rr[2]-rr[1])*0.1,b=1,lty=2)
+      abline(a=-(rr[2]-rr[1])*0.1,b=1,lty=2)
+      axis(2)
+      box()
+      qqplot(xi[treat==0 & matched],
+             xi[treat==1 & matched],
+             xlim=rr,ylim=rr,axes=F,ylab="",
+             xlab="")
+      abline(a=0,b=1)
+      abline(a=(rr[2]-rr[1])*0.1,b=1,lty=2)
+      abline(a=-(rr[2]-rr[1])*0.1,b=1,lty=2)
+      box()
+    } else{
+      xij <- jitter(xi)
+      rr <- range(xij)
+      qqplot(xij[treat==0],xij[treat==1],
+             xlim=rr,ylim=rr,axes=F,
+             xlab="",ylab="")
+      abline(a=0,b=1)
+      abline(a=(rr[2]-rr[1])*0.1,b=1,lty=2)
+      abline(a=-(rr[2]-rr[1])*0.1,b=1,lty=2)
+      axis(2)
+      box()
+      qqplot(xij[treat==0 & matched],
+             xij[treat==1 & matched],
+             xlim=rr,ylim=rr,axes=F,
+             xlab="",ylab="")
+      abline(a=0,b=1)
+      abline(a=(rr[2]-rr[1])*0.1,b=1,lty=2)
+      abline(a=-(rr[2]-rr[1])*0.1,b=1,lty=2)
+      box()
+    }
+  }  
+}
