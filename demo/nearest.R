f1cdaab4176ba0cafdb82ac2f855531689ccdb39
Elizabeth Stuart <estuart@jhsph.edu>
2005-07-13 23:00:25 +0000

    added user prompts

--- demo/nearest.R
+++ demo/nearest.R
@@ -2,33 +2,37 @@
 ### An Example Script for Nearest Neighbor Matching
 ###
 data(lalonde)
+cat("DEMO 2:1 Nearest neighbor matching \n")
 m.out <- matchit(treat ~ re74+re75+age+educ, data=lalonde,
                  method = "nearest", distance = "logit", ratio=2)
-cat("DEMO 2:1 Nearest neighbor matching \n")
 print(m.out)
 summary(m.out)
 
-m.out2 <- matchit(treat ~ re74+re75+age+educ, data=lalonde,
-                 method = "nearest", distance = "logit", mahvars=c("re74", "re75"), exact=c("married"))
+silent <- readline("\nPress <return> to continue: ")
 cat("DEMO 1:1 Nearest neighbor matching with Mahalanobis matching on re74 and re75 and exact matching on married \n")
+m.out2 <- matchit(treat ~ re74+re75+age+educ, data=lalonde,
+                 method = "nearest", distance = "logit", mahvars=c("re74", "re75"), exact=c("married"), caliper=.25)
 print(m.out2)
 summary(m.out2)
 
+silent <- readline("\nPress <return> to continue: ")
+cat("DEMO 1:1 Nearest neighbor matching with units outside the common support discarded \n")
 m.out3 <- matchit(treat ~ re74+re75+age+educ, data=lalonde,
                  method = "nearest", distance = "logit", discard="both")
-cat("DEMO 1:1 Nearest neighbor matching with units outside the common support discarded \n")
 print(m.out3)
 summary(m.out3)
 
+silent <- readline("\nPress <return> to continue: ")
+cat("DEMO 2:1 Nearest neighbor matching with replacement \n")
 m.out4 <- matchit(treat ~ re74+re75+age+educ, data=lalonde,
                  method = "nearest", distance = "logit", replace=TRUE, ratio=2)
-cat("DEMO 2:1 Nearest neighbor matching with replacement \n")
 print(m.out4)
 summary(m.out4)
 
+silent <- readline("\nPress <return> to continue: ")
+cat("DEMO 1:1 Nearest neighbor matching followed by subclassification \n")
 m.out5 <- matchit(treat ~ re74+re75+age+educ, data=lalonde,
                  method = "nearest", distance = "logit", subclass=5)
-cat("DEMO 1:1 Nearest neighbor matching followed by subclassification \n")
 print(m.out5)
 summary(m.out5)
 
