d37335189f9bc45c02c8c344915b7115782670dc
Kosuke Imai <kimai@Princeton.Edu>
2005-07-14 01:01:49 +0000

    user.prompt()

--- demo/nearest.R
+++ demo/nearest.R
@@ -8,28 +8,28 @@ m.out <- matchit(treat ~ re74+re75+age+educ, data=lalonde,
 print(m.out)
 summary(m.out)
 
-silent <- readline("\nPress <return> to continue: ")
+user.prompt()
 cat("DEMO 1:1 Nearest neighbor matching with Mahalanobis matching on re74 and re75 and exact matching on married \n")
 m.out2 <- matchit(treat ~ re74+re75+age+educ, data=lalonde,
                  method = "nearest", distance = "logit", mahvars=c("re74", "re75"), exact=c("married"), caliper=.25)
 print(m.out2)
 summary(m.out2)
 
-silent <- readline("\nPress <return> to continue: ")
+user.prompt()
 cat("DEMO 1:1 Nearest neighbor matching with units outside the common support discarded \n")
 m.out3 <- matchit(treat ~ re74+re75+age+educ, data=lalonde,
                  method = "nearest", distance = "logit", discard="both")
 print(m.out3)
 summary(m.out3)
 
-silent <- readline("\nPress <return> to continue: ")
+user.prompt()
 cat("DEMO 2:1 Nearest neighbor matching with replacement \n")
 m.out4 <- matchit(treat ~ re74+re75+age+educ, data=lalonde,
                  method = "nearest", distance = "logit", replace=TRUE, ratio=2)
 print(m.out4)
 summary(m.out4)
 
-silent <- readline("\nPress <return> to continue: ")
+user.prompt()
 cat("DEMO 1:1 Nearest neighbor matching followed by subclassification \n")
 m.out5 <- matchit(treat ~ re74+re75+age+educ, data=lalonde,
                  method = "nearest", distance = "logit", subclass=5)
