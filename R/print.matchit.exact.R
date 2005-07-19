b53fdf905e6a15ca1a0c37570778b1092cc7eccd
Elizabeth Stuart <estuart@jhsph.edu>
2005-07-19 17:46:54 +0000

    fixed summary and print for exact matching

--- R/print.matchit.exact.R
+++ R/print.matchit.exact.R
@@ -1,13 +1,13 @@
 print.matchit.exact <- function(x, digits = getOption("digits"), ...){
   cat("\nCall: ", deparse(x$call),"\n",sep = "")
-  cat("\nExact Subclasses: ", max(x$subclass),"\n",sep="")
+  cat("\nExact Subclasses: ", max(x$subclass, na.rm=T),"\n",sep="")
   cat("\nSample sizes:\n")
-  ntab <- table(factor(x$subclass!=0,
+  ntab <- table(factor(!is.na(x$subclass),
                        levels=c("TRUE","FALSE")),
                 x$treat)
   nn <- rbind(table(x$treat),
               ntab[c("TRUE","FALSE"),])
-  dimnames(nn) <- list(c("Full","Matched","Discarded"),
+  dimnames(nn) <- list(c("Full","Matched","Unmatched"),
                        c("Control","Treated"))
   print.table(nn, ...)
   invisible(x)
