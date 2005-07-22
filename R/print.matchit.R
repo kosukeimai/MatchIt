2003c18eab88c6caca35dc4060fce8477c5f7bd4
Elizabeth Stuart <estuart@jhsph.edu>
2005-07-22 21:16:05 +0000

    fixed printing for discard

--- R/print.matchit.R
+++ R/print.matchit.R
@@ -10,9 +10,11 @@ print.matchit <- function(x, digits = getOption("digits"), ...){
   #  nn <- rbind(table(x$treat),
   #              table(x$weights>0,x$treat)[2:1,])
 
-  if(sum(x$discarded)==0) { nn <- rbind(table(x$treat), table(x$weights>0, x$treat), c(0,0)) }
-  else { nn <- rbind(table(x$treat), table(x$weights>0,x$treat)[2:1,]) }
-  dimnames(nn) <- list(c("Full","Matched","Discarded"),
+  if(sum(x$discarded)==0) { nn <- rbind(table(x$treat), table(x$weights>0, x$treat)[2,],
+		table(x$weights>0, x$treat)[1,], c(0,0)) }
+  else { nn <- rbind(table(x$treat), table(x$weights>0,x$treat)[2,], 
+		table(x$weights>0, x$treat)[1,], table(x$discarded, x$treat)[2,]) }
+  dimnames(nn) <- list(c("Full","Matched","Unmatched","Discarded"),
                        c("Control","Treated"))
   print.table(nn, ...)
   invisible(x)
