886a4810fbc64e7848a3e31289eff70cbdd6c248
Elizabeth Stuart <estuart@jhsph.edu>
2005-07-23 20:08:14 +0000

    small changes

--- inst/doc/graphics.R
+++ inst/doc/graphics.R
@@ -1,29 +1,36 @@
 # File to create sample graphics and output for documentation
 
 library(MatchIt)
+library(lattice)
 data(lalonde)
 m.out <- matchit(treat ~ re74+re75+educ+black+hispan+age, data=lalonde, method="nearest")
 
 print(summary(m.out))
-#postscript("docs/figs/qqplotnn.ps")
-#par(mfrow=c(1,2))
-#plot(m.out)
-#dev.off()
+trellis.device(device="pdf",file="qqplotnn.pdf",color=FALSE,width=8,height=4)
+par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
+    mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white", mfrow=c(1,2))
+plot(m.out)
+dev.off()
 
-#postscript("docs/figs/jitterplotnn.ps")
-#plot(m.out, type="jitter")
-#dev.off()
+trellis.device(device="pdf",file="jitterplotnn.pdf",color=FALSE,width=8,height=4)
+par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
+    mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white")
+plot(m.out, type="jitter")
+dev.off()
 
 m.outs <- matchit(treat ~ re74+re75+educ+black+hispan+age, data=lalonde, method="subclass")
 print(summary(m.outs))
-#postscript("docs/figs/qqplotsub.ps")
-#par(mfrow=c(1,2))
-#plot(m.outs)
-#dev.off()
+trellis.device(device="pdf",file="qqplotsub.pdf",color=FALSE,width=8,height=4)
+par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
+    mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white", mfrow=c(1,2))
+plot(m.outs)
+dev.off()
 
-#postscript("docs/figs/jitterplotsubs.ps")
-#plot(m.outs, type="jitter")
-#dev.off()
+trellis.device(device="pdf",file="jitterplotsub.pdf",color=FALSE,width=8,height=4)
+par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
+    mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white")
+plot(m.outs, type="jitter")
+dev.off()
 
 
 
