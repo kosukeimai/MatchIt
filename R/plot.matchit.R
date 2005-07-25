bbecd2d56268076c99b3f28686bc3675734b28c5
Kosuke Imai <kimai@Princeton.Edu>
2005-07-25 01:43:24 +0000

    dos2unix

--- R/plot.matchit.R
+++ R/plot.matchit.R
@@ -1,5 +1,5 @@
 # Need to account for weights -- how do we do qq plots with weights
-plot.matchit <- function(x,discrete.cutoff=5,type="QQ",numdraws=5000){
+plot.matchit <- function(x, y = NULL, discrete.cutoff=5,type="QQ",numdraws=5000){
   if (class(x)[1]=="matchit.exact"){
     return(warning("Plot() not appropriate for exact matching.  No plots generated."))
   }
