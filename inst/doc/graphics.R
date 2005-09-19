effd63c95641e0405bb13476ec4b443e1766329b
Elizabeth Stuart <estuart@jhsph.edu>
2005-09-19 19:17:23 +0000

    did qqplots in sweave

--- inst/doc/graphics.R
+++ inst/doc/graphics.R
@@ -6,13 +6,15 @@ data(lalonde)
 m.out <- matchit(treat ~ re74+re75+educ+black+hispan+age, data=lalonde, method="nearest")
 
 print(summary(m.out))
-trellis.device(device="pdf",file="qqplotnn.pdf",color=FALSE,width=8,height=4)
+#trellis.device(device="pdf",file="qqplotnn.pdf",color=FALSE,width=8,height=4)
+pdf(file="figs/qqplotnn.pdf")
 par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
     mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white", mfrow=c(1,2))
 plot(m.out)
 dev.off()
 
-trellis.device(device="pdf",file="jitterplotnn.pdf",color=FALSE,width=8,height=4)
+#trellis.device(device="pdf",file="jitterplotnn.pdf",color=FALSE,width=8,height=4)
+pdf(file="figs/jitterplotnn.pdf")
 par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
     mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white")
 plot(m.out, type="jitter")
@@ -20,13 +22,15 @@ dev.off()
 
 m.outs <- matchit(treat ~ re74+re75+educ+black+hispan+age, data=lalonde, method="subclass")
 print(summary(m.outs))
-trellis.device(device="pdf",file="qqplotsub.pdf",color=FALSE,width=8,height=4)
+#trellis.device(device="pdf",file="qqplotsub.pdf",color=FALSE,width=8,height=4)
+pdf(file="figs/qqplotsub.pdf")
 par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
     mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white", mfrow=c(1,2))
 plot(m.outs)
 dev.off()
 
-trellis.device(device="pdf",file="jitterplotsub.pdf",color=FALSE,width=8,height=4)
+#trellis.device(device="pdf",file="jitterplotsub.pdf",color=FALSE,width=8,height=4)
+pdf(file="figs/jitterplotsub.pdf")
 par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
     mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white")
 plot(m.outs, type="jitter")
