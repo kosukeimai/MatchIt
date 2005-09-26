9cbeb2cd1f3c1839b1e3d2474d14facd5b7e97a2
Elizabeth Stuart <estuart@jhsph.edu>
2005-09-26 22:40:53 +0000

    fixed small things

--- inst/doc/graphics.R
+++ inst/doc/graphics.R
@@ -7,17 +7,23 @@ m.out <- matchit(treat ~ re74+re75+educ+black+hispan+age, data=lalonde, method="
 
 print(summary(m.out))
 #trellis.device(device="pdf",file="qqplotnn.pdf",color=FALSE,width=8,height=4)
-pdf(file="figs/qqplotnn.pdf")
+postscript(file="figs/qqplotnn1.eps")
 par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
     mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white", mfrow=c(1,2))
-plot(m.out)
+plot(m.out, which.xs=c("re74", "re75", "educ"), interactive=FALSE)
+dev.off()
+
+postscript(file="figs/qqplotnn2.eps")
+par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
+    mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white", mfrow=c(1,2))
+plot(m.out, which.xs=c("black", "hispan", "educ"), interactive=FALSE)
 dev.off()
 
 #trellis.device(device="pdf",file="jitterplotnn.pdf",color=FALSE,width=8,height=4)
-pdf(file="figs/jitterplotnn.pdf")
+postscript(file="figs/jitterplotnn.eps")
 par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
     mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white")
-plot(m.out, type="jitter")
+plot(m.out, type="jitter", interactive=FALSE)
 dev.off()
 
 m.outs <- matchit(treat ~ re74+re75+educ+black+hispan+age, data=lalonde, method="subclass")
