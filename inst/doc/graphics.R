6aa8cc1c944e66142dd74cb70ea687748be6926a
Elizabeth Stuart <estuart@jhsph.edu>
2005-09-28 19:54:23 +0000

    new figures--make them a set size

--- inst/doc/graphics.R
+++ inst/doc/graphics.R
@@ -6,21 +6,38 @@ data(lalonde)
 m.out <- matchit(treat ~ re74+re75+educ+black+hispan+age, data=lalonde, method="nearest")
 
 print(summary(m.out))
-#trellis.device(device="pdf",file="qqplotnn.pdf",color=FALSE,width=8,height=4)
-postscript(file="figs/qqplotnn1.eps")
+
+postscript(file="figs/qqplotnn1.eps", width=2.75, height=2.75)
 par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
     mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white", mfrow=c(1,2))
 plot(m.out, which.xs=c("re74", "re75", "educ"), interactive=FALSE)
 dev.off()
 
-postscript(file="figs/qqplotnn2.eps")
+pdf(file="figs/qqplotnn1.pdf", width=2.75, height=2.75)
+par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
+    mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white", mfrow=c(1,2))
+plot(m.out, which.xs=c("re74", "re75", "educ"), interactive=FALSE)
+dev.off()
+
+postscript(file="figs/qqplotnn2.eps", width=2.75, height=2.75)
 par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
     mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white", mfrow=c(1,2))
 plot(m.out, which.xs=c("black", "hispan", "educ"), interactive=FALSE)
 dev.off()
 
-#trellis.device(device="pdf",file="jitterplotnn.pdf",color=FALSE,width=8,height=4)
-postscript(file="figs/jitterplotnn.eps")
+pdf(file="figs/qqplotnn2.pdf", width=2.75, height=2.75)
+par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
+    mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white", mfrow=c(1,2))
+plot(m.out, which.xs=c("black", "hispan", "educ"), interactive=FALSE)
+dev.off()
+
+postscript(file="figs/jitterplotnn.eps", width=5.5, height=3.5)
+par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
+    mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white")
+plot(m.out, type="jitter", interactive=FALSE)
+dev.off()
+
+pdf(file="figs/jitterplotnn.pdf", width=5.5, height=3.5)
 par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
     mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white")
 plot(m.out, type="jitter", interactive=FALSE)
