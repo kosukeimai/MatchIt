d0723ff8ada8243ac0b6e190e45082249a377d54
Elizabeth Stuart <estuart@jhsph.edu>
2005-09-29 14:53:24 +0000

    new figures

--- inst/doc/graphics.R
+++ inst/doc/graphics.R
@@ -7,57 +7,42 @@ m.out <- matchit(treat ~ re74+re75+educ+black+hispan+age, data=lalonde, method="
 
 print(summary(m.out))
 
-postscript(file="figs/qqplotnn1.eps", width=2.75, height=2.75, horizontal=FALSE,paper="special")
+ps.options(family = c("Times"), pointsize = 12)
+
+postscript(file="figs/qqplotnn1.eps", horizontal=FALSE, paper="special", width=2.75, height=2.75)
 par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
     mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white", mfrow=c(1,2))
 plot(m.out, which.xs=c("re74", "re75", "educ"), interactive=FALSE)
 dev.off()
 
-pdf(file="figs/qqplotnn1.pdf", width=2.75, height=2.75)
+pdf(file="figs/qqplotnn1.pdf", width=2.75, height=2.75, pointsize=12, family="Times")
 par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
     mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white", mfrow=c(1,2))
 plot(m.out, which.xs=c("re74", "re75", "educ"), interactive=FALSE)
 dev.off()
 
-postscript(file="figs/qqplotnn2.eps", width=2.75, height=2.75, horizontal=FALSE, paper="special")
+postscript(file="figs/qqplotnn2.eps", horizontal=FALSE, paper="special", width=2.75, height=2.75)
 par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
     mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white", mfrow=c(1,2))
 plot(m.out, which.xs=c("black", "hispan", "educ"), interactive=FALSE)
 dev.off()
 
-pdf(file="figs/qqplotnn2.pdf", width=2.75, height=2.75)
+pdf(file="figs/qqplotnn2.pdf", width=2.75, height=2.75, pointsize=12, family="Times")
 par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
     mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white", mfrow=c(1,2))
 plot(m.out, which.xs=c("black", "hispan", "educ"), interactive=FALSE)
 dev.off()
 
-postscript(file="figs/jitterplotnn.eps", width=5.5, height=3.5, horizontal=FALSE, paper="special")
+postscript(file="figs/jitterplotnn.eps", horizontal=FALSE, paper="special", width=5.5, height=3.5)
 par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
     mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white")
 plot(m.out, type="jitter", interactive=FALSE)
 dev.off()
 
-pdf(file="figs/jitterplotnn.pdf", width=5.5, height=3.5)
+pdf(file="figs/jitterplotnn.pdf", width=5.5, height=3.5, pointsize=12, family="Times")
 par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
     mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white")
 plot(m.out, type="jitter", interactive=FALSE)
 dev.off()
-
-m.outs <- matchit(treat ~ re74+re75+educ+black+hispan+age, data=lalonde, method="subclass")
-print(summary(m.outs))
-#trellis.device(device="pdf",file="qqplotsub.pdf",color=FALSE,width=8,height=4)
-pdf(file="figs/qqplotsub.pdf")
-par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
-    mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white", mfrow=c(1,2))
-plot(m.outs)
-dev.off()
-
-#trellis.device(device="pdf",file="jitterplotsub.pdf",color=FALSE,width=8,height=4)
-pdf(file="figs/jitterplotsub.pdf")
-par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.7, cex.axis=0.5,
-    mgp=c(1,0.5,0), cex.main=0.8, cex=1, bg="white")
-plot(m.outs, type="jitter")
-dev.off()
-
 
 
