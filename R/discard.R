1e26b4b467795ef9e5cbcd14b1033ddb69999874
Kosuke Imai <kimai@Princeton.Edu>
2005-10-08 17:47:58 +0000

    changing the option names

--- R/discard.R
+++ R/discard.R
@@ -13,7 +13,7 @@ discard <- function(treat, pscore, option, X) {
     discarded <- (pscore < pmin1 | pscore > pmax1)
   else if (option == "treat")   # discard treated units only
     discarded <- (pscore < pmin0 | pscore > pmax0)
-  else if (any(grep(option, c("ch.control", "ch.treat", "ch.both")))) {
+  else if (any(grep(option, c("hull.control", "hull.treat", "hull.both")))) {
     ## convext hull stuff
     if (!("whatif" %in% .packages(all = TRUE)))
       install.packages("whatif", CRAN="http://gking.harvard.edu")
@@ -22,13 +22,13 @@ discard <- function(treat, pscore, option, X) {
     require(whatif)
     require(lpSolve)
     discarded <- rep(FALSE, n.obs)
-    if (option == "ch.control"){ # discard units not in T convex hull
+    if (option == "hull.control"){ # discard units not in T convex hull
       wif <- whatif(cfact = X[treat==0,], data = X[treat==1,])
       discarded[treat==0] <- !wif$in.hull
-    } else if (option == "ch.treat") {
+    } else if (option == "hull.treat") {
       wif <- whatif(cfact = X[treat==1,], data = X[treat==0,])
       discarded[treat==1] <- !wif$in.hull
-    } else if (option == "ch.both"){ # discard units not in T&C convex hull
+    } else if (option == "hull.both"){ # discard units not in T&C convex hull
       wif <- whatif(cfact = cbind(1-treat, X), data = cbind(treat, X))
       discarded <- !wif$in.hull
     }
