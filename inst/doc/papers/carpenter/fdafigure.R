ff97f8bcb3e825facb7d51288d3021b6dfde744b
Kosuke Imai <kimai@Princeton.Edu>
2005-11-02 20:21:36 +0000

    up

--- inst/doc/papers/carpenter/fdafigure.R
+++ inst/doc/papers/carpenter/fdafigure.R
@@ -2,6 +2,40 @@ rm(list=ls())
 library(lattice)
 load("matchfdaLin.RData")
 
+##
+## calculation of qoi
+##
+library(MASS)
+library(survival)
+
+## matched data
+mcall <- res$call
+mcall$data <- mdata
+mres <- eval(mcall)
+## original data
+mres <- res
+
+coef <- coef(mres)
+var <- vcov(mres)
+nsims <- 100000
+par <- mvrnorm(nsims, mu=c(coef(mres), log(mres$scale)), Sigma=vcov(mres))
+scale2 <- exp(par[,ncol(par)])^2
+par <- par[,-ncol(par)]
+
+x <- model.matrix(mres)
+y <- mres$y
+x0 <- x1 <- x[x[,"treat"] == 1,] # treated only
+x0[,"treat"] <- 0  # counterfactual for the treated
+y <- y[x[,"treat"] == 1,] # observed Y(1) for the treated
+
+att <- apply(ifelse(y[,2]>0.5, matrix(rep(exp(y[,1]), nsims), nrow=nsims, byrow=TRUE),
+                    exp(par%*%t(x1)+0.5*scale2)) -
+                    exp(par%*%t(x0)+0.5*scale2), 1, mean)
+cat("ATT: ", mean(att), " (", sd(att), ")", sep="")
+
+##
+## density figure
+##
 doverlay <- function(x1, x0, xlab = "", main = "", lines = FALSE,
                      leg=T, ...)
 {
@@ -18,8 +52,8 @@ doverlay <- function(x1, x0, xlab = "", main = "", lines = FALSE,
   }
 }
 
-pdf("fdadens.pdf", paper="special", height=3, width=6)
-par(mar=c(2, 2, 2, 2) + 0.1, cex.lab=0.8, cex.axis=0.8,
+pdf("fdadens.pdf", paper="special", height=3.5, width=6)
+par(mar=c(2.5, 2, 2, 2) + 0.1, cex.lab=0.8, cex.axis=0.8,
     mgp=c(1.5,0.5,0), cex.main=0.5, cex=0.8, bg="white")
 doverlay(mate,ate,lwd=2,
          xlab="Estimated in-sample average treatment effect", leg=F)
@@ -29,8 +63,10 @@ text(-42,0.05,"Matched\ndata")
 text(-52.5,0.14,"Point estimate of \n Carpenter's specification \n using raw data")
 dev.off()
 
+##
+## eCDF figure
+##
 sm <- summary(m.out, standardize=TRUE)
-
 pdf("eCDF.pdf")
 plot(x=sm$sum.all[1:19,"eCDF Max"], y=sm$sum.matched[1:19,"eCDF Max"],
      xlab="before matching", ylab="after matching", pch=19)
