ec1ed73741a71ef6cc57eaf5bbe756d5615d51ea
Elizabeth Stuart <estuart@jhsph.edu>
2007-10-02 18:01:06 +0000

    switched order of panels

--- R/hist.pscore.R
+++ R/hist.pscore.R
@@ -9,12 +9,12 @@ hist.pscore <- function(x){
   hist(pscore[treat==1],xlim=range(na.omit(pscore)),
        xlab="Propensity Score", freq=F,
        main="Raw Treated")
-  hist(pscore[treat==0],xlim=range(na.omit(pscore)),
-       xlab="Propensity Score", freq=F,
-       main="Raw Control")
   hist(pscore[treat==1 & weights!=0],xlim=range(na.omit(pscore)),
        xlab="Propensity Score", freq=F,
        main="Matched Treated")
+  hist(pscore[treat==0],xlim=range(na.omit(pscore)),
+       xlab="Propensity Score", freq=F,
+       main="Raw Control")
   hist(pscore[treat==0 & weights!=0],xlim=range(na.omit(pscore)),
        xlab="Propensity Score", freq=F,
        main="Matched Control")  
