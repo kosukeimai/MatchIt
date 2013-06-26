7180a426ea0f4a4a2b19d0adfacea79e38f47640
Kosuke Imai <kimai@Princeton.Edu>
2013-06-26 11:13:34 +0000

    changed to onAttach.R

--- R/load.first.R
+++
@@ -1,9 +0,0 @@
-.onAttach <- function(...) {
-  mylib <- dirname(system.file(package = "MatchIt"))
-  ver <- packageDescription("MatchIt", lib = mylib)$Version
-  builddate <- packageDescription("MatchIt", lib = mylib)$Date
-  cat(paste("## \n##  MatchIt (Version ", ver, ", built: ", builddate, ")\n", sep = "")) 
-  cat("##  Please refer to http://gking.harvard.edu/matchit for full documentation \n",
-      "##  or help.matchit() for help with commands supported by MatchIt.\n##\n",
-      sep="")
-}
