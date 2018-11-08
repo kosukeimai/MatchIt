34fdc9e7a0e47f6d2bd7d06613fbe2ad337b6f63
Stolz <simon.stolz@whu.edu>
2018-11-08 14:40:25 +0100

    added test for plot par mfrow reset

---
+++ tests/testthat/test-plotting.R
@@ -0,0 +1,19 @@
+
+library(testthat)
+library(MatchIt)
+data("lalonde")
+
+context("hist.pscore")
+
+test_that("hist.pscore correctly restores par() settings", {
+  # setup test
+  m.out <- matchit(treat ~ educ + black + hispan, data = lalonde,
+                   method = "nearest")
+  
+  par_prior <- par(no.readonly = TRUE)
+  plot(m.out, type="hist")
+  par_now <- par(no.readonly = TRUE)
+  
+  # tests
+  expect_equal(par_prior, par_now)
+})
