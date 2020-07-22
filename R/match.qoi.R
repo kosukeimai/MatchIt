94460069f91ad97ef5b5a4774b07cf99b972268b
Noah Greifer <noah.greifer@gmail.com>
2020-07-22 02:45:43 -0400

    Rewrite, mostly for organization. SD control is removed from output.

--- R/match.qoi.R
+++ R/match.qoi.R
@@ -1,86 +1,82 @@
 ## Function to calculate summary stats
-qoi <- function(xx,tt,ww, t.plot=NULL, c.plot=NULL, sds=NULL,
-                standardize = FALSE, std=F){
-  weighted.var <- function(x, w) {
-    sum(w * (x - weighted.mean(x,w))^2)/(sum(w) - 1)}
-  xsum <- matrix(NA,2,7)
-  xsum <- as.data.frame(xsum)
-  row.names(xsum) <- c("Full","Matched")
+qoi <- function(xx, tt, ww = NULL, s.d.denom = "treated", standardize = FALSE) {
+
+  full.only <- is.null(ww)
+
+  xsum <- matrix(NA_real_, nrow = 1, ncol = 6)
+  rownames(xsum) <- "Full"
   if (standardize)
-    names(xsum) <- c("Means Treated","Means Control",
-                     "SD Control", "Std. Mean Diff.",
-                     "eCDF Med", "eCDF Mean", "eCDF Max")
+    colnames(xsum) <- c("Means Treated","Means Control", "Std. Mean Diff.",
+                        "eCDF Med", "eCDF Mean", "eCDF Max")
   else
-    names(xsum) <- c("Means Treated","Means Control",
-                     "SD Control", "Mean Diff",
-                     "eQQ Med", "eQQ Mean", "eQQ Max")
+    colnames(xsum) <- c("Means Treated","Means Control", "Mean Diff",
+                        "eQQ Med", "eQQ Mean", "eQQ Max")
   x1 <- xx[tt==1]
   x0 <- xx[tt==0]
-  ww1 <- ww[tt==1]
-  ww0 <- ww[tt==0]
-  xsum[1,1] <- mean(x1,na.rm=T)
-  xsum[1,2] <- mean(x0,na.rm=T)
-  xsum[1,3] <- sd(x0,na.rm=T)
-  X.t.m <- xx[tt==1][ww1>0]
-  X.c.m <- xx[tt==0][ww0>0]
-  xsum[2,1] <- weighted.mean(X.t.m, ww1[ww1>0])
-  xsum[2,2] <- weighted.mean(X.c.m, ww0[ww0>0])
-  xsum[2,3] <- sqrt(weighted.var(X.c.m, ww0[ww0>0]))
-  if(!(sum(tt==1)<2|(sum(tt==0)<2))){ 
-    xsd <- sd(x1,na.rm=T)
-    qqall <- qqsum(x1,x0,standardize=standardize)
-    xsum[1,5:7] <- c(qqall$meddiff,qqall$meandiff,qqall$maxdiff)
-    if (standardize)
-      if (!is.null(sds))
-        xsum[1,4] <- (mean(x1,na.rm=T)-mean(x0,na.rm=T))/sds
-      else
-        xsum[1,4] <- (mean(x1,na.rm=T)-mean(x0,na.rm=T))/xsd
-    else
-      xsum[1,4] <- mean(x1,na.rm=T)-mean(x0,na.rm=T)
-    if(!is.null(t.plot))
-      qqmat <- qqsum(xx[t.plot],xx[c.plot],standardize=standardize)
-    else
-      qqmat <- qqsum(x1[ww1>0],x0[ww0>0],standardize=standardize)
-    xsum[2,5:7] <- c(qqmat$meddiff,qqmat$meandiff,qqmat$maxdiff)
-    if (standardize)
-      if (!is.null(sds))
-        xsum[2,4] <- (xsum[2,1]-xsum[2,2])/sds
-      else
-        xsum[2,4] <- (xsum[2,1]-xsum[2,2])/xsd
-    else
-      xsum[2,4] <- xsum[2,1]-xsum[2,2]
-  } 
-  if(!std){
-    xsum <- xsum[,c(1:2,4:7)]
+
+  too.small <- length(x1) < 2 || length(x0) < 2
+
+  xsum["Full","Means Treated"] <- mean(x1, na.rm=TRUE)
+  xsum["Full","Means Control"] <- mean(x0, na.rm=TRUE)
+
+  mdiff <- xsum["Full","Means Treated"] - xsum["Full","Means Control"]
+
+  if (standardize && abs(mdiff) > 1e-8) {
+    if (!too.small) {
+      std <- switch(s.d.denom,
+                    "treated" = sd(x0, na.rm = TRUE),
+                    "control" = sd(x0, na.rm = TRUE),
+                    "pooled" = sqrt(.5*(var(x1, na.rm = TRUE) + var(x0, na.rm = TRUE))),
+                    stop("s.d.denom must be one of \"treated\", \"control\", or \"pooled\".", call. = FALSE))
+
+      xsum["Full", 3] <- mdiff/std
+    }
+  }
+  else {
+    xsum["Full", 3] <- mdiff
+  }
+
+  if (!too.small) {
+    qqall <- qqsum(xx, tt, standardize = standardize)
+    xsum["Full", 4:6] <- qqall[c("meddiff", "meandiff", "maxdiff")]
+  }
+
+  if (!full.only) {
+    xsum.matched <- matrix(NA_real_, nrow = 1, ncol = 6)
+    rownames(xsum.matched) <- "Matched"
+    colnames(xsum.matched) <- colnames(xsum)
+
+    too.small <- sum(ww[tt==1] > 0) < 2 || sum(ww[tt==0] > 0) < 2
+
+    xsum.matched["Matched","Means Treated"] <- weighted.mean(x1, ww[tt==1], na.rm=TRUE)
+    xsum.matched["Matched","Means Control"] <- weighted.mean(x0, ww[tt==0], na.rm=TRUE)
+
+    if (!(length(x1) < 2 || (length(x0) < 2))) {
+
+      mdiff <- xsum.matched["Matched","Means Treated"] - xsum.matched["Matched","Means Control"]
+
+      if (standardize && abs(mdiff) > 1e-8) {
+        if (!too.small) {
+          std <- switch(s.d.denom,
+                        "treated" = sd(x0, na.rm = TRUE),
+                        "control" = sd(x0, na.rm = TRUE),
+                        "pooled" = sqrt(.5*(var(x1, na.rm = TRUE) + var(x0, na.rm = TRUE))),
+                        stop("s.d.denom must be one of \"treated\", \"control\", or \"pooled\".", call. = FALSE))
+
+          xsum.matched["Matched",3] <- mdiff/std
+        }
+      }
+      else {
+        xsum.matched["Matched",3] <- mdiff
+      }
+
+      if (!too.small) {
+        qqmat <- qqsum(xx, tt, ww, standardize = standardize)
+        xsum.matched["Matched", 4:6] <- qqmat[c("meddiff", "meandiff", "maxdiff")]
+      }
+
+    }
+    xsum <- rbind(xsum, xsum.matched)
   }
   xsum
-}
-
-## By subclass
-qoi.by.sub <- function(xx,tt,ww,qq,standardize=FALSE){
-  qbins <- max(qq,na.rm=TRUE)
-  q.table <- matrix(0,6,qbins)
-  qn <- matrix(0,3,qbins)
-  matched <- ww!=0
-  for (i in 1:qbins) {
-    qi <- qq[matched]==i & (!is.na(qq[matched]))
-    qx <- xx[matched][qi]
-    qt <- tt[matched][qi]
-    qw <- as.numeric(ww[matched][qi]!=0)
-    if(sum(qt==1)<2|(sum(qt==0)<2)){
-      if(sum(qt==1)<2)
-        warning("Not enough treatment units in subclass ",i,call.=FALSE)
-      else if(sum(qt==0)<2)
-        warning("Not enough control units in subclass ",i,call.=FALSE)
-    }
-    qoi.i <- qoi(qx,qt,qw, sds=sd(xx[tt==1],na.rm=T), standardize=standardize)
-    q.table[,i] <- as.numeric(qoi.i[1,])
-    qn[,i] <- c(sum(qt),sum(qt==0),length(qt))
-  }
-  q.table <- as.data.frame(q.table)
-  qn <- as.data.frame(qn)
-  names(q.table) <- names(qn) <- paste("Subclass",1:qbins)
-  row.names(q.table) <- names(qoi.i)
-  row.names(qn) <- c("Treated","Control","Total")
-  list(q.table=q.table,qn=qn)
 }
