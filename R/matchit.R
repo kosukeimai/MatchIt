5f37babb9b922bcb893f38ad4a21f14e436a3a8f
Noah Greifer <noah.greifer@gmail.com>
2020-07-22 02:46:50 -0400

    Near complete rewrite to support additional arguments and improve processing of inputs. Output has been standardized across methods

--- R/matchit.R
+++ R/matchit.R
@@ -1,114 +1,199 @@
 #' @export
-matchit <- function(formula, data, method = "nearest", distance = "logit",
-                    distance.options=list(), discard = "none",
-                    reestimate = FALSE, ...) { 
+matchit <- function(formula, data = NULL, method = "nearest", distance = "glm",
+                    link = "logit", distance.options=list(), exact = NULL, mahvars = NULL,
+                    discard = "none", replace = FALSE, m.order = NULL, caliper = NULL,
+                    ratio = 1, reestimate = FALSE, verbose = FALSE, ...) {
 
   #Checking input format
   #data input
   mcall <- match.call()
-  if(is.null(data)) stop("Dataframe must be specified",call.=FALSE)
-  if(!is.data.frame(data)){
-    stop("Data must be a dataframe",call.=FALSE)}
-  if(sum(is.na(data))>0)
-    stop("Missing values exist in the data")
-  # list-wise deletion
-  # allvars <- all.vars(mcall)
-  # varsindata <- colnames(data)[colnames(data) %in% all.vars(mcall)]
-  # data <- na.omit(subset(data, select = varsindata))
-  
-  ## 7/13/06: Convert character variables to factors as necessary
-  ischar <- rep(0, dim(data)[2])
-  for (i in 1:dim(data)[2]) 
-    if(is.character(data[,i])) data[,i] <- as.factor(data[,i])
-  
-  ## check inputs
-  if (!is.numeric(distance)) {
-    fn1 <- paste("distance2", distance, sep = "")
-    if (!exists(fn1))
-      stop(distance, "not supported.")
-  }
-  if (is.numeric(distance)) {
-    fn1 <- "distance2user"
-  }
-  fn2 <- paste("matchit2", method, sep = "")
-  if (!exists(fn2))
-    stop(method, "not supported.")
 
-  ## obtain T and X
-  tryerror <- try(model.frame(formula), TRUE)
-  if (is.character(distance) &&
-        distance %in% c("GAMlogit", "GAMprobit", "GAMcloglog", "GAMlog", "GAMcauchit")) {
-    requireNamespace("mgcv")
-    tt <- terms(mgcv::interpret.gam(formula)$fake.formula)
-  } else {
-    tt <- terms(formula)
-  }
-  attr(tt, "intercept") <- 0
-  mf <- model.frame(tt, data)
-  treat <- model.response(mf)
-  X <- model.matrix(tt, data=mf)
+  #Process formula and data inputs
+  if (!inherits(formula, "formula")) stop("formula must be a formula object.", call. = FALSE)
+  f.env <- environment(formula)
 
-  ## estimate the distance measure
-  if (method == "exact") {
-    distance <- out1 <- discarded <- NULL
-    if (!is.null(distance))
-      warning("distance is set to `NULL' when exact matching is used.")
-  } else if (is.numeric(distance)){
-    out1 <- NULL
-    discarded <- discard(treat, distance, discard, X)
-  } else {
-    if (is.null(distance.options$formula))
-      distance.options$formula <- formula
-    if (is.null(distance.options$data))
-      distance.options$data <- data
-    out1 <- do.call(fn1, distance.options)
-    discarded <- discard(treat, out1$distance, discard, X)
-    if (reestimate) {
-      distance.options$data <- data[!discarded,]
-      distance.options$weights <- distance.options$weights[!discarded]
-      tmp <- out1
-      out1 <- do.call(fn1, distance.options)
-      tmp$distance[!discarded] <- out1$distance
-      out1$distance <- tmp$distance
+  data0 <- get_all_vars(formula, data = data)
+  for (i in names(data0)[vapply(data0, is.character, logical(1L))]) data0[[i]] <- factor(data0[[i]])
+
+  treat.formula <- update(formula, . ~ 0)
+  treat.mf <- model.frame(treat.formula, data = data0, na.action = "na.pass")
+  treat <- model.response(treat.mf)
+  if (anyNA(treat)) stop("Missing values are not allowed in the treatment.", call. = FALSE)
+  names(treat) <- rownames(data0)
+
+  covs.formula <- delete.response(terms(formula))
+  covs <- get_all_vars(covs.formula, data = data0)
+  if (anyNA(covs)) stop("Missing values are not allowed in the covariates.", call. = FALSE)
+
+  n.obs <- length(treat)
+
+  ## Process method
+  if (length(method) == 1 && is.atomic(method)) {
+    method <- as.character(method)
+    fn2 <- paste0("matchit2", method)
+    if (!exists(fn2)) stop(paste0("\"", method, "\" is not a supported method."), call. = FALSE)
+  }
+  else {
+    stop("method must be the name of a supported matching method. See ?matchit for allowable options.", call. = FALSE)
+  }
+
+  #Process distance and discard
+  check.inputs(method, distance, mcall, exact, mahvars, caliper, discard, replace, ratio, m.order)
+
+  exactcovs <- mahcovs <- NULL
+
+  if (method %in% c("exact", "cem")) {
+    distance <- fn1 <- NULL
+  }
+  else {
+    distance <- process.distance(distance, method)
+
+    if (is.numeric(distance)) {
+      if (length(distance) != length(treat)) stop("distance must be the same length as the dataset.", call. = FALSE)
+      if (anyNA(distance)) stop("Missing values are not allowed in distance.", call. = FALSE)
+      fn1 <- "distance2user"
     }
-    distance <- out1$distance
+    else {
+      fn1 <- paste0("distance2", distance)
+    }
+
+    if (!is.null(exact)) {
+      if (is.character(exact)) {
+        if (is.null(data) || !is.data.frame(data)) {
+          stop("If exact is specified as strings, a data frame containing the named variables must be supplied to data.", call. = FALSE)
+        }
+        if (!all(exact %in% names(data))) {
+          stop("All names supplied to exact must be variables in data.", call. = FALSE)
+        }
+        exact <- reformulate(exact)
+      }
+      else if (inherits(exact, "formula")) {
+        exact <- update(exact, NULL ~ .)
+      }
+      else {
+        stop("exact must be supplied as a character vector of names or a one-sided formula.", call. = FALSE)
+      }
+      exactcovs <- get_all_vars(exact, data)
+    }
+
+    if (!is.null(mahvars)) {
+      if (is.character(mahvars)) {
+        if (is.null(data) || !is.data.frame(data)) {
+          stop("If mahvars is specified as strings, a data frame containing the named variables must be supplied to data.", call. = FALSE)
+        }
+        if (!all(mahvars %in% names(data))) {
+          stop("All names supplied to mahvars must be variables in data.", call. = FALSE)
+        }
+        mahvars <- reformulate(mahvars)
+      }
+      else if (inherits(mahvars, "formula")) {
+        mahvars <- update(mahvars, NULL ~ .)
+      }
+      else {
+        stop("exact must be supplied as a character vector of names or a one-sided formula.", call. = FALSE)
+      }
+      mahcovs <- get_all_vars(mahvars, data)
+    }
   }
 
-  ## full mahalanobis matching
-  if(fn1=="distance2mahalanobis"){
-    is.full.mahalanobis <- TRUE
-  } else {is.full.mahalanobis <- FALSE}
+  is.full.mahalanobis <- identical(fn1, "distance2mahalanobis")
+
+  #Estimate distance, discard form common support, optionally re-estimate distance
+
+  if (is.null(fn1)) {
+    #Only for cem and exact
+    dist.model <- NULL
+    discarded <- rep(FALSE, length(treat))
+  }
+  else if (fn1 == "distance2user") {
+    dist.model <- NULL
+    discarded <- discard(treat, distance, discard, covs)
+  }
+  else if (fn1 == "distance2mahalanobis") {
+    distance <- NULL
+    dist.model <- NULL
+    discarded <- discard(treat, distance, discard, covs)
+  }
+  else {
+    #Estimate distance
+    if (is.null(distance.options$formula)) distance.options$formula <- formula
+    if (is.null(distance.options$data)) distance.options$data <- data
+    if (is.null(distance.options$verbose)) distance.options$verbose <- verbose
+    if (!is.null(attr(distance, "link"))) distance.options$link <- attr(distance, "link")
+    else distance.options$link <- link
+
+    dist.out <- do.call(fn1, distance.options, quote = TRUE)
+
+    dist.model <- dist.out$model
+    distance <- dist.out$distance
+
+    #Discard
+    discarded <- discard(treat, dist.out$distance, discard, covs)
+
+    #Optionally reestimate
+    if (reestimate && any(discarded)) {
+      for (i in seq_along(distance.options)) {
+        if (length(distance.options[[i]]) == n.obs) {
+          distance.options[[i]] <- distance.options[[i]][!discarded]
+        }
+        else if (length(dim(distance.options[[i]])) == 2 && nrow(distance.options[[i]]) == n.obs) {
+          distance.options[[i]] <- distance.options[[i]][!discarded,,drop = FALSE]
+        }
+      }
+      dist.out <- do.call(fn1, distance.options, quote = TRUE)
+      dist.model <- dist.out$model
+      distance[!discarded] <- dist.out$distance
+    }
+  }
+
+
+  # ## obtain T and X
+  # tryerror <- try(model.frame(formula), TRUE)
+  # if (is.character(distance) &&
+  #       distance %in% c("GAMlogit", "GAMprobit", "GAMcloglog", "GAMlog", "GAMcauchit")) {
+  #   requireNamespace("mgcv")
+  #   tt <- terms(mgcv::interpret.gam(formula)$fake.formula)
+  # } else {
+  #   tt <- terms(formula)
+  # }
+  # attr(tt, "intercept") <- 0
+  # mf <- model.frame(tt, data)
+  # treat <- model.response(mf)
+  # X <- model.matrix(tt, data=mf)
 
   ## matching!
-  out2 <- do.call(fn2, list(treat, X, data, distance=distance, discarded,
-                            is.full.mahalanobis=is.full.mahalanobis, ...)) 
-
-  ## no distance for full mahalanobis matching
-  if(fn1=="distance2mahalanobis"){
-    distance[1:length(distance)] <- NA
-    class(out2) <- c("matchit.mahalanobis","matchit")
-  } 
-
-  ## putting all the results together
-  out2$call <- mcall
-  out2$model <- out1$model
-  out2$formula <- formula
-  out2$treat <- treat
-  if (is.null(out2$X)){
-    out2$X <- X
-  }
-  out2$distance <- distance
-  out2$discarded <- discarded
+  match.out <- do.call(fn2, list(treat = treat, covs = covs, data = data, distance = distance,
+                                 discarded = discarded, exact = exact, mahvars = mahvars,
+                                 replace = replace, m.order = m.order, caliper = caliper,
+                                 ratio = ratio, is.full.mahalanobis = is.full.mahalanobis,
+                                 formula = formula, ...), quote = TRUE)
 
   ## basic summary
   nn <- matrix(0, ncol=2, nrow=4)
-  nn[1,] <- c(sum(out2$treat==0), sum(out2$treat==1))
-  nn[2,] <- c(sum(out2$treat==0 & out2$weights>0), sum(out2$treat==1 & out2$weights>0))
-  nn[3,] <- c(sum(out2$treat==0 & out2$weights==0 & out2$discarded==0), sum(out2$treat==1 & out2$weights==0 & out2$discarded==0))
-  nn[4,] <- c(sum(out2$treat==0 & out2$weights==0 & out2$discarded==1), sum(out2$treat==1 & out2$weights==0 & out2$discarded==1))
+  nn[1,] <- c(sum(treat==0), sum(treat==1))
+  nn[2,] <- c(sum(treat==0 & match.out$weights > 0), sum(treat==1 & match.out$weights > 0))
+  nn[3,] <- c(sum(treat==0 & match.out$weights==0 & !discarded), sum(treat==1 & match.out$weights==0 & !discarded))
+  nn[4,] <- c(sum(treat==0 & discarded), sum(treat==1 & discarded))
   dimnames(nn) <- list(c("All","Matched","Unmatched","Discarded"),
-                       c("Control","Treated"))
-  out2$nn <- nn
-  
-  return(out2)
+                       c("Control", "Treated"))
+
+  #Create X.list for X output, removing duplicate variables
+  X.list <- list(covs, exactcovs, mahcovs)
+  all.covs <- lapply(X.list, names)
+  for (i in seq_along(X.list)[-1]) if (!is.null(X.list[[i]])) X.list[[i]][names(X.list[[i]]) %in% unlist(all.covs[1:(i-1)])] <- NULL
+  X.list[lengths(X.list) == 0] <- NULL
+
+  ## putting all the results together
+  match.out$model <- dist.model
+  match.out$X <- do.call("data.frame", X.list)
+  match.out$call <- mcall
+  match.out$formula <- formula
+  match.out$treat <- treat
+  match.out$distance <- distance
+  match.out$discarded <- discarded
+  match.out$exact <- exact
+  match.out$mahvars <- mahvars
+  match.out$nn <- nn
+
+  return(match.out)
 }
