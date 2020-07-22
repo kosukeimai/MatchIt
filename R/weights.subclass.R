a98bb28e7a4bf719e2f52f424f8c5ad465723049
Noah Greifer <noah.greifer@gmail.com>
2020-07-22 02:49:24 -0400

    Cleaning

--- R/weights.subclass.R
+++ R/weights.subclass.R
@@ -1,18 +1,18 @@
-weights.subclass <- function(psclass, treat) {
+weights.subclass <- function(psclass, treat, estimand = "ATT") {
 
   ttt <- treat[!is.na(psclass)]
-  classes <- na.omit(psclass)
-  
+  classes <- psclass[!is.na(psclass)]
+
   n <- length(ttt)
   labels <- names(ttt)
   tlabels <- labels[ttt==1]
   clabels <- labels[ttt==0]
-  
+
   weights <- rep(0, n)
   names(weights) <- labels
   weights[tlabels] <- 1
- 
-  for(j in unique(classes)){
+
+  for (j in unique(classes)){
     qn0 <- sum(ttt==0 & classes==j)
     qn1 <- sum(ttt==1 & classes==j)
     weights[ttt==0 & classes==j] <- qn1/qn0
@@ -30,14 +30,14 @@ weights.subclass <- function(psclass, treat) {
     names(tmp) <- names(treat[is.na(psclass)])
     weights <- c(weights, tmp)[names(treat)]
   }
-  
-  if (sum(weights)==0) 
+
+  if (sum(weights)==0)
     stop("No units were matched")
   else if (sum(weights[tlabels])==0)
     stop("No treated units were matched")
   else if (sum(weights[clabels])==0)
     stop("No control units were matched")
-  
+
   return(weights)
 }
 
