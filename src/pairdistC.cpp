71b4e4024e87b6e1734bb708c0d591dbca8eefc4
Noah Greifer <noah.greifer@gmail.com>
2020-09-17 03:41:10 -0400

    Cleaning

--- src/pairdistC.cpp
+++ src/pairdistC.cpp
@@ -1,16 +1,6 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp
-// function (or via the Source button on the editor toolbar). Learn
-// more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//   http://gallery.rcpp.org/
-//
-
 // [[Rcpp::export]]
 double pairdistsubC(const NumericVector& x_,
                     const IntegerVector& t_,
