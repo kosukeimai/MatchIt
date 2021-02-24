3a62636893781cfef7b258c3c4a0dc274dd6e49c
Noah Greifer <noah.greifer@gmail.com>
2021-02-23 20:31:12 -0500

    Added rbind methods for matchdata and getmatches objects (which now have their own classes)

--- R/match.data.R
+++ R/match.data.R
@@ -1,9 +1,10 @@
 match.data <- function(object, group = "all", distance = "distance", weights = "weights", subclass = "subclass",
-                       data = NULL, include.s.weights = TRUE, drop.unmatched = TRUE) {
+                               data = NULL, include.s.weights = TRUE, drop.unmatched = TRUE) {
 
   if (!inherits(object, "matchit")) {
     stop("'object' must be a matchit object, the output of a call to matchit().", call. = FALSE)
   }
+
   if (is.null(data)) {
     if (!is.null(object$model)) {
       env <- attributes(terms(object$model))$.Environment
@@ -75,15 +76,18 @@ match.data <- function(object, group = "all", distance = "distance", weights = "
   if (!is.null(object$weights)) attr(data, "weights") <- weights
   if (!is.null(object$subclass)) attr(data, "subclass") <- subclass
 
+  class(data) <- c("matchdata", class(data))
+
   return(data)
 }
 
 get_matches <- function(object, distance = "distance", weights = "weights", subclass = "subclass",
-                        id = "id", data = NULL, include.s.weights = TRUE) {
+                                id = "id", data = NULL, include.s.weights = TRUE) {
 
   if (!inherits(object, "matchit")) {
     stop("'object' must be a matchit object, the output of a call to matchit().", call. = FALSE)
   }
+
   if (is.null(object$match.matrix)) {
     stop("A match.matrix component must be present in the matchit object, which does not occur with all types of matching. Use match.data() instead.", call. = FALSE)
   }
@@ -138,5 +142,79 @@ get_matches <- function(object, distance = "distance", weights = "weights", subc
   attr(out, "subclass") <- subclass
   attr(out, "id") <- id
 
+  class(out) <- c("getmatches", class(out))
+
   return(out)
 }
+
+rbind.matchdata <- function(..., deparse.level = 1) {
+  allargs <- list(...)
+  allargs <- allargs[lengths(allargs) > 0L]
+  if (is.null(names(allargs))) md_list <- allargs
+  else md_list <- allargs[names(allargs) == ""]
+
+  allargs[names(allargs) == ""] <- NULL
+  allargs$deparse.level <- deparse.level
+
+  type <- intersect(c("matchdata", "getmatches"), unlist(lapply(md_list, class)))
+  if (length(type) == 0) stop("A matchdata or getmatches object must be supplied.", call. = FALSE)
+  else if (length(type) == 2) stop("Supplied objects must be all matchdata objects or all getmatches objects.", call. = FALSE)
+
+  attrs <- c("distance", "weights", "subclass", "id")
+  attr_list <- setNames(vector("list", length(attrs)), attrs)
+  key_attrs <- setNames(rep(NA_character_, length(attrs)), attrs)
+
+  for (i in attrs) {
+    attr_list[[i]] <- unlist(lapply(md_list, function(m) {
+      a <- attr(m, i)
+      if (length(a) == 0) NA_character_ else a
+    }))
+    if (all(is.na(attr_list[[i]]))) attr_list[[i]] <- NULL
+    else key_attrs[i] <- attr_list[[i]][which(!is.na(attr_list[[i]]))[1]]
+  }
+  attrs <- names(attr_list)
+  key_attrs <- key_attrs[attrs]
+
+  #Check if all non-attr columns are the same across datasets
+  other_col_list <- lapply(seq_along(md_list), function(d) {
+    setdiff(names(md_list[[d]]), unlist(lapply(attr_list, `[`, d)))
+  })
+  for (d in seq_along(md_list)[-1]) {
+    if (length(other_col_list[[d]]) != length(other_col_list[[1]]) || !all(other_col_list[[d]] %in% other_col_list[[1]])) {
+      stop(paste("The", switch(type, "matchdata" = "match.data()", "get_matches()"), "inputs must come from the same dataset."), call. = FALSE)
+    }
+  }
+
+  for (d in seq_along(md_list)) {
+    for (i in attrs) {
+      #Rename columns of each attribute the same across datasets
+      if (is.null(attr(md_list[[d]], i))) md_list[[d]] <- setNames(cbind(md_list[[d]], NA), c(names(md_list[[d]]), key_attrs[i]))
+      else names(md_list[[d]])[names(md_list[[d]]) == attr_list[[i]][d]] <- key_attrs[i]
+
+      #Give subclasses unique values across datasets
+      if (i == "subclass") {
+        if (all(is.na(md_list[[d]][[key_attrs[i]]]))) md_list[[d]][[key_attrs[i]]] <- factor(md_list[[d]][[key_attrs[i]]], levels = NA)
+        else levels(md_list[[d]][[key_attrs[i]]]) <- paste(d, levels(md_list[[d]][[key_attrs[i]]]), sep = "_")
+      }
+    }
+
+    #Put all columns in the same order
+    if (d > 1) {
+      md_list[[d]] <- md_list[[d]][names(md_list[[1]])]
+    }
+    class(md_list[[d]]) <- class(md_list[[d]])[class(md_list[[d]]) != type]
+  }
+
+
+
+  out <- do.call("rbind", c(md_list, allargs))
+
+  for (i in attrs) {
+    attr(out, i) <- unname(key_attrs[i])
+  }
+
+  class(out) <- c(type, class(out))
+
+  out
+}
+rbind.getmatches <- rbind.matchdata
