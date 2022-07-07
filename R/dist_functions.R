d711dbe29da2922e598036f11a19e268140cae64
Noah Greifer <noah.greifer@gmail.com>
2022-07-07 14:33:33 -0400

    Fixed bug in robust_mahal when discard is present

--- R/dist_functions.R
+++ R/dist_functions.R
@@ -80,10 +80,10 @@ transform_covariates <- function(formula = NULL, data = NULL, method = "mahalano
     #Rosenbaum (2010, ch8)
     X_r <- matrix(0, nrow = sum(!discarded), ncol = ncol(X),
                   dimnames = list(rownames(X)[!discarded], colnames(X)))
-    for (i in seq_len(ncol(X_r))) X_r[,i] <- rank(X[,i])
+    for (i in seq_len(ncol(X_r))) X_r[,i] <- rank(X[!discarded, i])
 
-    if (is.null(s.weights)) var_r <- cov(X_r[!discarded,, drop = FALSE])
-    else var_r <- cov.wt(X_r[!discarded,, drop = FALSE], s.weights[!discarded])$cov
+    if (is.null(s.weights)) var_r <- cov(X_r)
+    else var_r <- cov.wt(X_r, s.weights[!discarded])$cov
 
     multiplier <- sd(seq_len(sum(!discarded)))/sqrt(diag(var_r))
     var_r <- var_r * outer(multiplier, multiplier, "*")
@@ -95,7 +95,7 @@ transform_covariates <- function(formula = NULL, data = NULL, method = "mahalano
 
     if (any(discarded)) {
       X_r <- array(0, dim = dim(X), dimnames = dimnames(X))
-      for (i in seq_len(ncol(X_r))) X_r[,i] <- rank(X[,i])
+      for (i in seq_len(ncol(X_r))) X_r[!discarded,i] <- rank(X[!discarded,i])
     }
 
     X <- mahalanobize(X_r, inv_var)
