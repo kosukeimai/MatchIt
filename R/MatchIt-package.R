a737c3052dd82abdc5f0e4f346dec25109c66cb6
Noah Greifer <noah.greifer@gmail.com>
2022-11-13 01:56:37 -0500

    Added roxygen

---
+++ R/MatchIt-package.R
@@ -0,0 +1,19 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom grDevices devAskNewPage
+#' @importFrom grDevices nclass.FD
+#' @importFrom grDevices nclass.scott
+#' @importFrom grDevices nclass.Sturges
+#' @importFrom Rcpp evalCpp
+#' @importFrom utils capture.output
+#' @importFrom utils combn
+#' @importFrom utils setTxtProgressBar
+#' @importFrom utils txtProgressBar
+#' @import stats
+#' @import graphics
+#' @useDynLib MatchIt,.registration = TRUE
+#'
+## usethis namespace: end
+NULL
